40bfb32bb21bc55d491d47b6a6f93fd1d5bc4255
William Yao <williamyaoh@gmail.com>
2016-10-07 14:43:03 -0500

    add interface file

---
+++ ts/forms.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="jquery.d.ts" />
+declare function showProgressAnim(selector: string): void;
+declare function showSuccessMsg(selector: string, msg: string): void;
+declare function showErrorMsg(selector: string, msg: string): void;
