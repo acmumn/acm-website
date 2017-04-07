b4dfc5bc97db4787859f4374c381e5e812fae27a
William Yao <williamyaoh@gmail.com>
2017-04-06 19:10:20 -0500

    fix issue with compiling outside source directory

--- _plugins/jekyll_ts.rb
+++ _plugins/jekyll_ts.rb
@@ -1,8 +1,8 @@
 # Converts typescript files to javascript
 #
 # Uses the following configs
-#		 tsc => path of typescript compiler,
-#		 js_dest => output of js files after compilation
+#    tsc => path of typescript compiler,
+#    js_dest => output of js files after compilation
 
 # Written by Matt Sheehan, 2015
 
@@ -15,69 +15,68 @@ require 'open3'
 
 module Jekyll
 
-	class TsGenerator < Generator
-		safe true
-		priority :high
+  class TsGenerator < Generator
+    safe true
+    priority :high
 
-		def generate(site)
-			# location of typescript compiler
-			# defaults to tsc assuming in system env path
-			tsc = site.config["tsc"] || "tsc"
+    def generate(site)
+      # location of typescript compiler
+      # defaults to tsc assuming in system env path
+      tsc = site.config["tsc"] || "tsc"
 
-			# ts source
-			ts_src = site.config["ts-src"] || "/"
+      # ts source
+      ts_src = site.config["ts-src"] || "/"
 
-			# js destination
-			js_dest = site.config["js-dest"] || "/"
+      # js destination
+      js_dest = site.config["js-dest"] || "/"
 
-			ts_files = Array.new;
+      ts_files = Array.new;
 
-			site.static_files.delete_if do |sf|
-				next if not File.extname(sf.path) == ".ts"
-				
-				# get the dirname of file, but we don't need the site source
-				ts_dir = File.dirname(sf.path.gsub(site.source, ""))
-				ts_name = File.basename(sf.path)
+      site.static_files.delete_if do |sf|
+        next if not File.extname(sf.path) == ".ts"
+        
+        # get the dirname of file, but we don't need the site source
+        ts_dir = File.dirname(sf.path.gsub("#{site.source}/", ""))
+        ts_name = File.basename(sf.path)
 
-				# add ts file
-				ts_files << TsFile.new(site, site.source, ts_dir, ts_name, ts_src, js_dest, tsc)
-				# return true so this file gets removed from static_files
-				# we'll replace it with our own tsfile that implements
-				# it's own write
-				true
-			end
+        # add ts file
+        ts_files << TsFile.new(site, site.source, ts_dir, ts_name, ts_src, js_dest, tsc)
+        # return true so this file gets removed from static_files
+        # we'll replace it with our own tsfile that implements
+        # it's own write
+        true
+      end
 
-			# concat new tsfiles with static files
-			site.static_files.concat(ts_files)
-		end
-	end
+      # concat new tsfiles with static files
+      site.static_files.concat(ts_files)
+    end
+  end
 
+  class TsFile < StaticFile
+    def initialize(site, base, dir, name, tsroot, jsroot, tsc)
+      super(site, base, dir, name, nil)
 
-	class TsFile < StaticFile
-		def initialize(site, base, dir, name, tsroot, jsroot, tsc)
-			super(site, base, dir, name, nil)
+      @tspath = File::join dir, name
+      @tsdir = tsroot
+      @jsdir = jsroot
+      @tsc = tsc
+    end
 
-			@tspath = File.join tsroot, name
-			@tsdir = tsroot
-			@jsdir = jsroot
-			@tsc = tsc
-		end
+    def write(dest)
+      # js name
+      ts_ext = /\.ts$/i
+      js_name = @name.gsub(ts_ext, ".js")
 
-		def write(dest)
-			# js name
-			ts_ext = /\.ts$/i
-			js_name = @name.gsub(ts_ext, ".js")
-
-			# js full path
-			# js = File.join(js_path, js_name)
-      dest = File::basename dest
+      # js full path
+      # js = File.join(js_path, js_name)
       js_path = File::join dest, @jsdir
 
-			# make sure dir exists
-			FileUtils.mkdir_p(js_path)
-			# execute shell command
-			begin
-				command = "#{@tsc} -t ES5 --rootDir #{@tsdir} --outDir #{js_path} #{@tspath}"
+      # make sure dir exists
+      FileUtils.mkdir_p(js_path)
+      # execute shell command
+      begin
+        command = "#{@tsc} -t ES5 --rootDir #{@tsdir} --outDir #{js_path} #{@tspath}"
+        puts command
 
         stdout_str, stderr_str = Open3::capture3 command
 
@@ -85,7 +84,7 @@ module Jekyll
         if not stdout_str.empty? then STDERR.puts stdout_str end
         if not stderr_str.empty? then STDERR.puts stderr_str end
         stdout_str
-			end
-		end
-	end
+      end
+    end
+  end
 end
